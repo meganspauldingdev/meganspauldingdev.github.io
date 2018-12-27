045fbf916e7166a9c50edd4a4c415dabd09a5b90
Megan Spaulding <meganspauldingcreative@gmail.com>
2018-12-27 15:07:08 -0600

    Added in Hero Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainNav from './components/MainNav.js'
+import Hero from './components/Hero.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MainNav />
+        <Hero />
       </div>
     );
   }
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+
+	render() {
+		return(
+			<div id="hero">
+				<h1>DESIGN THAT SELLS</h1>
+			</div>
+		)
+	}
+
+}
+
+export default Hero
