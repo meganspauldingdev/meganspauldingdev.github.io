a2f77ab941d2fc241ded0ed7c0931e0db30b420f
Megan Spaulding <meganspauldingcreative@gmail.com>
2018-12-28 10:25:05 -0600

    Added biography component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import MainNav from './components/MainNav.js'
 import Hero from './components/Hero.js'
+import Biography from './components/Biography.js'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <MainNav />
         <Hero />
+        <Biography />
       </div>
     );
   }
---
+++ src/components/Biography.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Biography extends Component {
+
+	render() {
+		return(
+			<div id="biography">
+				<p>Dessert cheesecake toffee. Donut cake lemon drops sugar plum candy canes gummies candy chocolate cake chocolate bar. Candy canes dessert cookie marzipan. Candy canes dessert sugar plum candy dragée sweet chocolate cake macaroon cookie.</p>
+			</div>
+		)
+	}
+
+}
+
+export default Biography
