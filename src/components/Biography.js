68556e0cb553752c02acff97da36fb4fbd86018f
Megan Spaulding <meganspauldingcreative@gmail.com>
2018-12-28 18:47:50 -0600

    Styled biography component

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -5,7 +5,7 @@ class Biography extends Component {
 	render() {
 		return(
 			<div id="biography">
-				<p>Dessert cheesecake toffee. Donut cake lemon drops sugar plum candy canes gummies candy chocolate cake chocolate bar. Candy canes dessert cookie marzipan. Candy canes dessert sugar plum candy dragée sweet chocolate cake macaroon cookie.</p>
+				<p>Topping dessert chupa chups gingerbread. Danish tootsie roll jelly-o. Pudding bonbon lemon drops powder jelly carrot cake danish. Marzipan jelly-o powder marshmallow soufflé pastry dessert. Sweet roll candy canes caramels biscuit. Soufflé cupcake cake. Muffin cheesecake oat cake oat cake gingerbread donut chocolate soufflé sesame snaps. Wafer ice cream biscuit sesame snaps soufflé fruitcake candy danish. Muffin pie candy canes jelly-o jelly beans soufflé bear claw jelly-o sweet. Oat cake macaroon pastry dragée.</p>
 			</div>
 		)
 	}
