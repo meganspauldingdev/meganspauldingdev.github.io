5fe7691a7ddfbef3b7f9aa52fecc1ae473e97fa6
Megan Spaulding <meganspauldingcreative@gmail.com>
2018-12-29 09:30:32 -0600

    Added photos to services component.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import brandingCover from '../img/branding-cover.jpg';
+import webDesignCover from '../img/webdesign-cover.jpg';
+import webDevelopmentCover from '../img/web-development-cover.jpg';
 
 class Services extends Component {
 
@@ -8,9 +11,18 @@ class Services extends Component {
 				<h3>SERVICES</h3>
 				<hr/>
 				<div className="service-container">
-					<div className="service">1</div>
-					<div className="service">2</div>
-					<div className="service">3</div>
+					<div className="service">
+						<img src={brandingCover} alt="Branding"/>
+						<h2>Branding</h2>
+					</div>
+					<div className="service">
+						<img src={webDesignCover} alt="Web Design"/>
+						<h2>Web Design</h2>
+					</div>
+					<div className="service">
+						<img src={webDevelopmentCover} alt="Web Development"/>
+						<h2>Web Development</h2>
+					</div>
 				</div>
 			</section>
 		)
