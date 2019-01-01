27d4e46bd01c93877615f70db720827ecdaad5a8
Megan Spaulding <meganspauldingcreative@gmail.com>
2019-01-01 17:42:31 -0600

    Added logic to add triangle styles.

--- src/components/TriangleBackground.js
+++ src/components/TriangleBackground.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
+	const trianglePositions = [
+		{id: "triangle1", top: 50, left: "50px"},
+		{id: "triangle2", top: 100, left: "100px"},
+		{id: "triangle3", top: 500, left: "500px"},
+		{id: "triangle4", top: 1000, left: "1000px"},
+		{id: "triangle5", top: 253, left: "500px"},
+		{id: "triangle6", top: 1000, left: "50px"}
+		]
+
 class TriangleBackground extends Component {
 
+
+	styleTriangles = (triangles) => {
+		triangles.forEach(function(triangle){
+			let triangleID = triangle.id;
+			let el = document.getElementById(triangleID);
+			el.style.top = triangle.top + "px";
+			el.style.left = triangle.left;
+		}
+		)
+	}
+
+	componentDidMount() {
+		this.styleTriangles(trianglePositions);
+	}
+
 	render() {
+
 		return(
-			<div class="triangle">
-			
+
+
+			<div>
+				{trianglePositions.map((triangle, key) => (
+					<div className="triangle" id={triangle.id}></div>
+				))}
 			</div>
 		)
+
 	}
 
 }
--- src/components/WorkSample.js
+++ src/components/WorkSample.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 class WorkSample extends Component {
 
 	render() {
-		const testThis = this.props.sample.imageURL;
-		console.log(testThis);
 		return(
 			<div className="work-sample">
 				<img src={this.props.sample.imageURL} alt={this.props.sample.name}/>
