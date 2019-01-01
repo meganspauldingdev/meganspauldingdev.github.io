1e312b68ed0154dcb566133d7e5e9bd43e247baf
Megan Spaulding <meganspauldingcreative@gmail.com>
2019-01-01 17:57:53 -0600

    Added logic to rotate triangles for pattern.

--- src/components/TriangleBackground.js
+++ src/components/TriangleBackground.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 	const trianglePositions = [
-		{id: "triangle1", top: 50, left: "50px"},
-		{id: "triangle2", top: 100, left: "100px"},
-		{id: "triangle3", top: 500, left: "500px"},
-		{id: "triangle4", top: 1000, left: "1000px"},
-		{id: "triangle5", top: 253, left: "500px"},
-		{id: "triangle6", top: 1000, left: "50px"}
+		{id: "triangle1", top: 100, left: 50, transform: "rotate(-50deg)"},
+		{id: "triangle2", top: 25, left: 600, transform: "rotate(10deg)"},
+		{id: "triangle3", top: 50, left: 1200, transform: "rotate(170deg)"},
+		{id: "triangle4", top: 500, left: 350, transform: "rotate(-10deg)"},
+		{id: "triangle5", top: 400, left: 900, transform: "rotate(60deg)"},
+		{id: "triangle6", top: 900, left: 25, transform: "rotate(10deg)"}
 		]
 
 class TriangleBackground extends Component {
@@ -17,7 +17,8 @@ class TriangleBackground extends Component {
 			let triangleID = triangle.id;
 			let el = document.getElementById(triangleID);
 			el.style.top = triangle.top + "px";
-			el.style.left = triangle.left;
+			el.style.left = triangle.left + "px";
+			el.style.transform = triangle.transform;
 		}
 		)
 	}
