829b8b755cc2425c4357603e2257b85d3c0ff08e
Megan Spaulding <meganspauldingcreative@gmail.com>
2019-01-02 18:22:33 -0600

    Styled About Section

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,15 @@ class Portfolio extends Component {
 	render() {
 		return(
 			<section id="about">
-				<h3>Hey There!</h3>
-				<hr/>
-				<div id="bio">
-					<p>Topping dessert chupa chups gingerbread. Danish tootsie roll jelly-o. Pudding bonbon lemon drops powder jelly carrot cake danish. Marzipan jelly-o powder marshmallow soufflé pastry dessert. Sweet roll candy canes caramels biscuit. Soufflé cupcake cake. Muffin cheesecake oat cake oat cake gingerbread donut chocolate soufflé sesame snaps. Wafer ice cream biscuit sesame snaps soufflé fruitcake candy danish. Muffin pie candy canes jelly-o jelly beans soufflé bear claw jelly-o sweet. Oat cake macaroon pastry dragée.</p>
+					<h3>Hey There!</h3>
+					<hr/>
+				<div id="about-container">
+					<div id="about-content">
+						<p>Topping dessert chupa chups gingerbread. Danish tootsie roll jelly-o. Pudding bonbon lemon drops powder jelly carrot cake danish. Marzipan jelly-o powder marshmallow soufflé pastry dessert. Sweet roll candy canes caramels biscuit. Soufflé cupcake cake. Muffin cheesecake oat cake oat cake gingerbread donut chocolate soufflé sesame snaps. Wafer ice cream biscuit sesame snaps soufflé fruitcake candy danish. Muffin pie candy canes jelly-o jelly beans soufflé bear claw jelly-o sweet. Oat cake macaroon pastry dragée.</p>
+						<h5>Read More</h5>
+					</div>
+					<img src={Headshot} alt="Megan Spaulding"/>
 				</div>
-				<img src={Headshot}/>
 			</section>
 		)
 	}
