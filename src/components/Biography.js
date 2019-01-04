3bcc010f2f2af5f896dcf74b9f4b31aa8685f7f5
Megan Spaulding <meganspauldingcreative@gmail.com>
2019-01-04 00:46:29 -0600

    Added scroll animation to biography section.

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 
 class Biography extends Component {
 
+	componentDidMount() {
+		window.addEventListener('scroll', this.enterSide);
+	}
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.enterSide);
+	}
+
+	enterSide = () => {
+		const biography = document.getElementById('biography');
+		let scroll = window.scrollY;
+		if(scroll < 400){
+			biography.style.marginLeft = (-100+(scroll/4))+'%';
+			console.log(scroll);
+		}
+	}
+
 	render() {
 		return(
 			<div id="biography">
